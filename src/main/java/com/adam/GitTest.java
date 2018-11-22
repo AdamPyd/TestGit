64787ea37cc5654e1ffbf27f077972fbc7c53a38
Adam <wb-pyd489343@antfin.com>
2018-11-22 16:04:23 +0800

    Class GitTest modify for test node template

---
+++ src/main/java/com/adam/GitTest.java
@@ -0,0 +1,23 @@
+package com.adam;
+
+/**
+ * @Project: gittest
+ * @ClassName: GitTest
+ * @Description: 测试注释的格式
+ * @Author: Adam
+ * @Version: 1.1.0
+ * @Create: 2018-11-22 15:49
+ **/
+public class GitTest {
+    
+    /**
+      * @Description: 测试方法的注释模板
+      * @Param: [args] 
+      * @Author: Adam 
+      * @return: void 
+      * @Date: 2018/11/22 15:57
+      */ 
+    public static void main(String[] args) {
+        
+    }
+}
